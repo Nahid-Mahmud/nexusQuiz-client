99cd8d9b047d6c403ac7002c57620829583df5fc
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-12-21 20:08:23 +0600

    style: height and width adjusted of animation

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,13 +4,13 @@ import Login from "../Login/Login";
 const Home = () => {
   return (
     <div className="bg-slate-100">
-      <div className="flex flex-col-reverse md:flex-row max-w-[95vw] mx-auto min-h-[90vh]  items-center justify-around">
+      <div className="flex flex-col-reverse gap-5 lg:gap-0 md:flex-row max-w-[95vw] mx-auto min-h-[90vh]  items-center justify-around">
         <div className="text-center">
-          <h1 className="md:text-5xl text-3xl font-bold  max-w-md">
+          <h1 className="md:text-5xl text-3xl font-bold  max-w-lg">
             {" "}
-            Welcome to Nexus Quiz
+            Welcome to Nexus Quiz !
           </h1>
-          <p className="py-6 max-w-md">
+          <p className="py-6 max-w-lg">
             Your Gateway to Knowledge and Fun! Immerse yourself in a world of
             quizzes that challenge, entertain, and enlighten. Explore a diverse
             range of topics, test your intellect, and enjoy the thrill of
@@ -19,7 +19,7 @@ const Home = () => {
         </div>
         <div>
           <iframe
-            className="md:h-96 md:w-96 h-80 "
+            className="lg:h-[35rem] lg:w-[35rem]  h-80 "
             src="https://lottie.host/embed/876f897e-c2e0-431b-b21c-3b4dc33ee892/jdDH6X9FL0.json"
           ></iframe>
         </div>
